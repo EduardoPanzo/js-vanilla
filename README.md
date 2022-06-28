1700e4775c55dd9e405166a54f4232383dd1c072
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2022-06-28 06:47:25 +0100

    Estrutura inicial com alguns componete iniciais e de teste adicionados

---
+++ README.md
@@ -0,0 +1,11 @@
+# JavaScript vanilla Apps
+Uma app feito em javaScript puro, contruido com vite
+
+## Requisitos para o uso
+    Ter o node instalado pelo meno a versão 12
+    Executar o npm para instalar os pacotes e as depêndecias 
+    executar a App com os seguintes comando :
+        npm run dev - para modo devs
+        npm run build - para o modo de produção
+
+## Funcionamento da App
---
+++ main.js
@@ -0,0 +1,27 @@
+import './style.css'
+
+const app =document.querySelector('#app');
+
+app.innerHTML = `
+<div class="container">
+  <div class="appLiking">
+      Tornar links...
+      <a href="./src/calculator/index.html">Calculadora</a>
+  </div>
+  <div class="appLiking">
+      Conversor
+  </div>
+  <div class="appLiking">
+      <a href="./src/stopwatch/index.html">Crónometro</a>
+  </div>
+  <div class="appLiking">
+      Todo List
+  </div>
+  <div class="appLiking">
+      Todo List
+  </div>
+  <div class="appLiking">
+      Todo List
+  </div>
+</div>
+`
